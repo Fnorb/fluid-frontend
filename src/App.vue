91f816b8f76c9678253460a61faf581f09fd71f5
Fnorb <azaca@yahoo.com>
2025-07-17 00:30:14 +0200

    feat: add dummy content and square background

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="min-h-screen bg-green-900 text-white">
-    <Header/>
-    <Content/>
+    <Header />
+    <GooeySquares />
+    <Content />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from './components/Header.vue'
+import GooeySquares from './components/GooeySquares.vue';
 import Content from './components/Content.vue'
 </script>
