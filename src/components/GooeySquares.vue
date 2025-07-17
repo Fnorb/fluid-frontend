14865bac9188fc2461e36b5896d80072b06c4c9f
Arkadij Liebau <azaca@yahoo.com>
2025-07-17 09:55:42 +0200

    feat: add backstop js

---
+++ src/components/GooeySquares.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="z-0 pointer-events-none inset-0 absolute"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+interface Cube {
+    x: number
+    y: number
+    rotation: number
+}
+
+const NUMBER_OF_CUBES = 5
+const cubes = ref<Cube[]>([])
+</script>
