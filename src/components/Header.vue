7ce892cd4604355fe63619ef8574b2f0eb20fd5d
Fnorb <azaca@yahoo.com>
2025-07-25 16:34:04 +0200

    feat (header): update styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,9 @@
 <template>
-  <header class="fixed top-0 left-0 w-full bg-teal-300 h-16 z-30">
+  <header class="fixed top-0 left-0 w-full bg-[linear-gradient(3deg,_var(--color-teal-300)_40%,_#ccfbf1)] h-16 z-30">
     <div class="max-w-screen-lg mx-auto h-full flex items-center px-4">
-      <img src="/src/assets/logo4.webp" alt="Logo" class="!h-8 w-auto mr-3" />
-      <h1 class="text-xl font-semibold text-content-left">Practice Project 25-07-17</h1>
+      <h1
+        class="text-xl font-semibold text-content-left text-white drop-shadow-[0_0_5px_rgba(200,255,255,0.3)] mix-blend-plus-lighter opacity-100">
+        Portfolio 25-07-17</h1>
     </div>
   </header>
 </template>
