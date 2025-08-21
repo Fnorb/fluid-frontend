44cf6e8ad6a4f5d2cd3b5bda2071053712c9472a
Fnorb <azaca@yahoo.com>
2025-08-21 16:04:01 +0200

    style (ContentLinks): improve readable when linebreaks occur

--- src/components/ContentLinks.vue
+++ src/components/ContentLinks.vue
@@ -1,26 +1,25 @@
 <template>
     <section class="relative">
-
-        <!-- 1 col on small screens, 2 cols from md up -->
-        <dl class="grid gap-x-6 gap-y-3
-             [grid-template-columns:1fr]
-             md:[grid-template-columns:max-content_minmax(0,1fr)]">
-
-            <!-- iterate through the prop data to create the link list -->
+        <dl class="grid gap-x-6 gap-y-1 md:gap-y-3
+         [grid-template-columns:1fr]
+         md:[grid-template-columns:max-content_minmax(0,1fr)]">
             <template v-for="element in data" :key="element.value">
                 <dt class="font-medium text-left">
                     {{ element.label }}
                 </dt>
-                <dd class="text-gray-300 min-w-0">
+
+                <dd class="text-gray-300 min-w-0 mb-6 last:mb-0 md:mb-0">
                     <a :href="element.value" target="_blank" rel="noopener noreferrer" class="underline underline-offset-2 decoration-dotted hover:decoration-solid
-           whitespace-normal break-words [overflow-wrap:anywhere]">
+               whitespace-normal break-words [overflow-wrap:anywhere]">
                         {{ element.value }}
                     </a>
                 </dd>
             </template>
         </dl>
+
     </section>
 </template>
+
 
 <script setup lang="ts">
 interface Data {
